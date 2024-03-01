0d19be052a64774dcdf075655a0cd46c509b2ae7
MShaterhosein <MShaterhosein@IRN-LT-DVLPR3>
2024-03-01 14:32:30 +0330

    Initialize the react-router with router config

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router";
 
 const container = document.querySelector("#root");
 const root = createRoot(container);
 
-root.render(<h1>My Shop</h1>);
+root.render(<RouterProvider router={router}/>);
