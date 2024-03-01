a0921c377bbbe3992deddc8e94a73c6510b76ede
MShaterhosein <MShaterhosein@IRN-LT-DVLPR3>
2024-03-01 21:11:02 +0330

    Add product details link to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProducts } from "../api/fakeStore";
-
+import { Link } from "react-router-dom";
 const Home = () => {
   const query = useQuery({ queryKey: ["products"], queryFn: getProducts });
   return (
     <div>
       <ul>
-        {query.data?.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
+        {query.data?.map((product) => (
+          <div key={product.id}>
+            <li>{product.title}</li>
+            <Link to={`/details/${product.id}`}>Details</Link>
+          </div>
         ))}
       </ul>
     </div>
