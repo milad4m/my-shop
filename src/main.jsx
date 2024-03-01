c1ceae28ddd74c094879c52fbc23af9924e49354
MShaterhosein <MShaterhosein@IRN-LT-DVLPR3>
2024-03-01 19:45:10 +0330

    Initialize react-query with config with QueryClientProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
-
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./config/query";
 const container = document.querySelector("#root");
 const root = createRoot(container);
 
-root.render(<RouterProvider router={router}/>);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>,
+);
