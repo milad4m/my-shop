bd0c40fdb37e089b8a08a46c650833b094c959fb
MShaterhosein <MShaterhosein@IRN-LT-DVLPR3>
2024-03-01 21:10:13 +0330

    Add productId prams to the details page

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/detail",
+        path: "/details/:productId",
         element: <ProductDetail />,
       },
       {
