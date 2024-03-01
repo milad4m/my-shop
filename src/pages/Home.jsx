a320189413662fb4ea7031d80d3ca1c39e57e68a
MShaterhosein <MShaterhosein@IRN-LT-DVLPR3>
2024-03-01 19:46:47 +0330

    Show Products name in home page and cache data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,17 @@
-const Home = () => {};
+import { useQuery } from "@tanstack/react-query";
+import { getProducts } from "../api/fakeStore";
+
+const Home = () => {
+  const query = useQuery({ queryKey: ["products"], queryFn: getProducts });
+  return (
+    <div>
+      <ul>
+        {query.data?.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Home;
