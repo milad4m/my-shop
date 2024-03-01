ec1677b397ceff3d492bebd86cb0c8501e9c0891
MShaterhosein <MShaterhosein@IRN-LT-DVLPR3>
2024-03-01 19:45:57 +0330

    Fix fakeStoreApi to get products

--- src/api/fakeStore.js
+++ src/api/fakeStore.js
@@ -1,13 +1,19 @@
 const baseUrl = import.meta.env.VITE_FAKESTORE_API_URL;
 const getProducts = async () => {
   const response = await fetch(`${baseUrl}/products`);
-  const data = response.json;
+  if (!response.ok) {
+    throw new Error(`products fetch not ok`);
+  }
+  const data = response.json();
   return data;
 };
 
 const getProductDetail = async (id) => {
   const response = await fetch(`${baseUrl}/products/${id}`);
-  const data = response.json;
+  if (!response.ok) {
+    throw new Error(`productDetails/${id} fetch not ok`);
+  }
+  const data = response.json();
   return data;
 };
 export { getProducts, getProductDetail };
