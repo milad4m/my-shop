904c6bdb6b82e2b90cf2b56da4ee7d4743073d47
MShaterhosein <MShaterhosein@IRN-LT-DVLPR3>
2024-03-01 21:36:38 +0330

    Install and add ReactQueryDevtools to the project

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import queryClient from "./config/query";
 const container = document.querySelector("#root");
 const root = createRoot(container);
@@ -9,5 +10,6 @@ const root = createRoot(container);
 root.render(
   <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>,
 );
