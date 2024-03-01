91131be40b572d814eb872415eb14cd59a034072
MShaterhosein <MShaterhosein@IRN-LT-DVLPR3>
2024-03-01 21:12:37 +0330

    Show product title in product details page

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,3 +1,15 @@
-const ProductDetail = () => {};
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { getProductDetail } from "../api/fakeStore";
+
+const ProductDetail = () => {
+  const { productId } = useParams();
+  const query = useQuery({
+    queryKey: ["productDetail", productId],
+    queryFn: getProductDetail,
+  });
+  console.log(query.data);
+  return <div key={query.data?.id}>{query.data?.title}</div>;
+};
 
 export default ProductDetail;
