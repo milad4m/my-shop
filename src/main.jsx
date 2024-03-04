7bf2ea148dbed288770da891ace5b77c30ba74e5
MShaterhosein <MShaterhosein@IRN-LT-DVLPR3>
2024-03-04 17:58:40 +0330

    Add Redux toolkit provider to project

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import router from "./router/router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import queryClient from "./config/query";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 const container = document.querySelector("#root");
 const root = createRoot(container);
 
 root.render(
   <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-    <ReactQueryDevtools initialIsOpen={false} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </Provider>
   </QueryClientProvider>,
 );
