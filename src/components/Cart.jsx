512bcdae7cfc37791b3f54af41c0f2ac3e1b2172
MShaterhosein <MShaterhosein@IRN-LT-DVLPR3>
2024-03-06 18:55:31 +0330

    Implement Cart component

---
+++ src/components/Cart.jsx
@@ -0,0 +1,78 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  removeItem,
+  increaseItem,
+  decreaseItem,
+  getTotalQuantity,
+} from "../store/slices/productSlice";
+const Cart = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getTotalQuantity());
+  }, [dispatch]);
+  const items = useSelector((state) => state.counter.items);
+  return (
+    <div className="overflow-y-auto h-screen">
+      <h2>Cart:</h2>
+      <ul className="container grid gap-2 grid-cols-1">
+        {items.map((item) => (
+          <li
+            key={item.id}
+            className="text-center flex flex-col justify-around items-center"
+          >
+            <img
+              className="h-32 lg:h-48 mx-auto"
+              src={item.image}
+              alt={item.title}
+            />
+            <h3 className="font-bold	m-3">{item.title}</h3>
+            <div>
+              <>
+                <button
+                  className="font-bold text-lg text-blue-700 p-5"
+                  onClick={() =>
+                    dispatch(
+                      increaseItem({
+                        title: item.title,
+                        id: item.id,
+                        image: item.image,
+                      }),
+                    )
+                  }
+                >
+                  +
+                </button>
+                <span>{item.itemQuantity}</span>
+                <button
+                  className="font-bold text-lg text-red-700 p-5"
+                  onClick={() =>
+                    item.itemQuantity == 1
+                      ? dispatch(
+                          removeItem({
+                            title: item.title,
+                            id: item.id,
+                            image: item.image,
+                          }),
+                        )
+                      : dispatch(
+                          decreaseItem({
+                            title: item.title,
+                            id: item.id,
+                            image: item.image,
+                          }),
+                        )
+                  }
+                >
+                  {item.itemQuantity == 1 ? "remove" : "-"}
+                </button>
+              </>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Cart;
