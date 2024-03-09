7188fb5b77728901c6ebf5321c0790eb67848b38
MShaterhosein <MShaterhosein@IRN-LT-DVLPR3>
2024-03-09 20:14:41 +0330

    Refactor ProductDetails based on button component

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,16 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { getProductDetails } from "../api/fakeStore";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  addItem,
-  removeItem,
-  increaseItem,
-  decreaseItem,
-} from "../store/slices/productSlice";
+import { useSelector } from "react-redux";
+import Button from "../components/Button";
 const ProductDetails = () => {
   const { productId } = useParams();
-  const dispatch = useDispatch();
   const { data } = useQuery({
     queryKey: ["productDetail", productId],
     queryFn: getProductDetails,
@@ -18,76 +12,46 @@ const ProductDetails = () => {
   const items = useSelector((state) => state.counter.items);
   const quantity = items.find((item) => item.id === data?.id)?.itemQuantity;
   const totalQuantity = useSelector((state) => state.counter.totalQuantity);
+  const item = { title: data?.title, id: data?.id, image: data?.image };
   return (
     <div className="col-span-5 container">
       <p className="mb-4">total Quantity : {totalQuantity}</p>
       <div className="text-center flex flex-col justify-around items-center">
         <img
           className="h-32 lg:h-80 mx-auto"
-          src={data?.image}
-          alt={data?.category}
+          src={item.image}
+          alt={item.title}
         />
         <h3 key={data?.id} className="font-bold	m-3">
-          {data?.title}
+          {item.title}
         </h3>
         <div>
           {quantity ? (
             <>
-              <button
-                className="font-bold text-lg text-blue-700 p-5"
-                onClick={() =>
-                  dispatch(
-                    increaseItem({
-                      title: data?.title,
-                      id: data?.id,
-                      image: data?.image,
-                    }),
-                  )
-                }
+              <Button
+                eventName="increase"
+                classList="font-bold text-lg text-blue-700 p-5"
+                item={item}
               >
                 +
-              </button>
+              </Button>
               <span>{quantity}</span>
-              <button
-                className="font-bold text-lg text-red-700 p-5"
-                onClick={
-                  quantity == 1
-                    ? () =>
-                        dispatch(
-                          removeItem({
-                            title: data?.title,
-                            id: data?.id,
-                            image: data?.image,
-                          }),
-                        )
-                    : () =>
-                        dispatch(
-                          decreaseItem({
-                            title: data?.title,
-                            id: data?.id,
-                            image: data?.image,
-                          }),
-                        )
-                }
+              <Button
+                eventName={quantity == 1 ? "remove" : "decrease"}
+                item={item}
+                classList="font-bold text-lg text-red-700 p-5"
               >
                 {quantity == 1 ? "remove" : "-"}
-              </button>
+              </Button>
             </>
           ) : (
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              onClick={() =>
-                dispatch(
-                  addItem({
-                    title: data?.title,
-                    id: data?.id,
-                    image: data?.image,
-                  }),
-                )
-              }
+            <Button
+              eventName="Add"
+              classList="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              item={item}
             >
-              add to cart
-            </button>
+              Add To Cart
+            </Button>
           )}
         </div>
       </div>
