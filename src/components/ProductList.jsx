9be53ae4090f69b95128005b31c7a6d9242cb9ae
MShaterhosein <MShaterhosein@IRN-LT-DVLPR3>
2024-03-10 08:52:43 +0330

    use TotalQuantity component in ProductList component

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,15 +2,14 @@ import Product from "./Product";
 import { useQuery } from "@tanstack/react-query";
 import { getProducts } from "../api/fakeStore";
 import { useSelector } from "react-redux";
-
+import TotalQuantity from "./TotalQuantity";
 const ProductList = () => {
   const { data } = useQuery({ queryKey: ["products"], queryFn: getProducts });
   const items = useSelector((state) => state.counter.items);
-  const totalQuantity = useSelector((state) => state.counter.totalQuantity);
   return (
     <>
       {console.log(items)}
-      <p>total Quantity : {totalQuantity}</p>
+      <TotalQuantity />
       <ul className="container grid gap-2 xl:gap-3 grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {data?.map((product) => (
           <Product
